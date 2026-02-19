8d89ddf16b2f08f3413d4ad1c7fcf4ef5692c56b
Mohammed Azarin vp <azarcoder@gmail.com>
2026-02-19 07:05:05 +0530

    agent AI

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use("/api/chat", chatRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
